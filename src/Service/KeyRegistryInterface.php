dbe7ee8404d851f0939950e8b29742bebdafad6b
Florian Engelhardt <florian.engelhardt@oxid-esales.com>
2019-10-15 09:10:22 +0200

    OXDEV-2642 - update comments

--- src/Service/KeyRegistryInterface.php
+++ src/Service/KeyRegistryInterface.php
@@ -7,15 +7,11 @@
 namespace OxidEsales\GraphQl\Service;
 
 /**
- * Class KeyRegistry
- *
- * The current implementation stores the signature key in
- * the config table. This should be changed eventually.
- *
  * @package OxidEsales\GraphQl\Service
  */
 interface KeyRegistryInterface
 {
     public function generateSignatureKey(): string;
+
     public function getSignatureKey(): string;
 }
