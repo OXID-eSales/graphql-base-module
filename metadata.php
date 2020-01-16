f120036455ebea186475466747af2c2c694fe0d0
Florian Engelhardt <florian.engelhardt@oxid-esales.com>
2020-01-16 13:09:22 +0100

    prepare next release

--- metadata.php
+++ metadata.php
@@ -27,7 +27,7 @@ $aModule = [
         'en'      => '<span>OXID GraphQL API Framework</span>',
     ],
     'thumbnail'   => 'out/pictures/logo.png',
-    'version'     => '1.3.1',
+    'version'     => '1.3.2',
     'author'      => 'OXID eSales',
     'url'         => 'www.oxid-esales.com',
     'email'       => 'info@oxid-esales.com',
