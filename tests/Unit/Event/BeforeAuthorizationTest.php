e93804a93faf4a5910a15cdcf3ef93c17bb1598e
Florian Engelhardt <florian.engelhardt@oxid-esales.com>
2020-05-15 09:03:01 +0200

    OXDEV-3466 Add missing unit test for BeforeAuthorization event

---
+++ tests/Unit/Event/BeforeAuthorizationTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+declare(strict_types=1);
+
+namespace OxidEsales\GraphQL\Base\Tests\Unit\Event;
+
+use OxidEsales\GraphQL\Base\Event\BeforeAuthorization;
+use OxidEsales\GraphQL\Base\Framework\NullToken;
+use PHPUnit\Framework\TestCase;
+
+class BeforeAuthorizationEvent extends TestCase
+{
+    public function testBasicGetters(): void
+    {
+        $event = new BeforeAuthorization(
+            new NullToken(),
+            'right'
+        );
+
+        $this->assertInstanceOf(
+            NullToken::class,
+            $event->getToken()
+        );
+        $this->assertSame(
+            'right',
+            $event->getRight()
+        );
+        $this->assertNull(
+            $event->getAuthorized()
+        );
+    }
+}
