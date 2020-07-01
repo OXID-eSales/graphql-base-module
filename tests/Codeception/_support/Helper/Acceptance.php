e41ddc926a7b928723b8fdc27cb11e9a5cf1534a
Florian Engelhardt <florian.engelhardt@oxid-esales.com>
2020-07-01 16:16:44 +0200

    remove dependency on oxid codeception modules lib

--- tests/Codeception/_support/Helper/Acceptance.php
+++ tests/Codeception/_support/Helper/Acceptance.php
@@ -7,11 +7,16 @@
 
 declare(strict_types=1);
 
-namespace Helper;
+namespace OxidEsales\GraphQL\Base\Tests\Codeception\Helper;
+
+use OxidEsales\Facts\Facts;
 
 // here you can define custom actions
 // all public methods declared in helper class will be available in $I
 
 class Acceptance extends \Codeception\Module
 {
+    public function _beforeSuite($settings = []) {
+        exec((new Facts())->getShopRootPath() . '/bin/oe-console oe:module:activate oe/graphql-base');
+    }
 }
