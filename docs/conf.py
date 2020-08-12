ad885d2373d799fd026b61c991b990c156898656
Florian Engelhardt <florian.engelhardt@oxid-esales.com>
2020-08-12 12:46:32 +0200

    OXDEV-3825 Add analytics id

--- docs/conf.py
+++ docs/conf.py
@@ -99,7 +99,9 @@ html_context = {
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'analytics_id': 'UA-57247309-8',
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = ['_themes']
