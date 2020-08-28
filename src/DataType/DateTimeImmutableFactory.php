0ce43c6b4108521835acf6a70eb27c4d5c5b313f
j.skoczek <j.skoczek@createit.pl>
2020-08-28 11:18:58 +0200

    OXDEV-3922 added nullable DateTimeImmutableFactory which uses named constructors

---
+++ src/DataType/DateTimeImmutableFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+declare(strict_types=1);
+
+namespace OxidEsales\GraphQL\Base\DataType;
+
+use DateTimeImmutable;
+use DateTimeZone;
+
+final class DateTimeImmutableFactory
+{
+    public static function fromString(string $time = 'now', ?DateTimeZone $timezone = null): ?DateTimeImmutable
+    {
+        if (strlen($time) == 0 || strpos($time, '0000-00-00') !== false) {
+            return null;
+        }
+
+        return new DateTimeImmutable($time, $timezone);
+    }
+
+    public static function fromTimeStamp(int $timeStamp): ?DateTimeImmutable
+    {
+        if ($timeStamp <= 0) {
+            return null;
+        }
+
+        return (new DateTimeImmutable())->setTimestamp($timeStamp);
+    }
+}
