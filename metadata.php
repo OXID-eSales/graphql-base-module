02ae1a7256ee1f5f555cb840d98cb9c8c60126c2
Florian Engelhardt <flo@dotbox.org>
2020-09-03 11:30:40 +0200

    prepare 4.0.0 release

--- metadata.php
+++ metadata.php
@@ -26,7 +26,7 @@ $aModule = [
         'en'      => '<span>OXID GraphQL API Framework</span>',
     ],
     'thumbnail'   => 'out/pictures/logo.png',
-    'version'     => '3.2.0',
+    'version'     => '4.0.0',
     'author'      => 'OXID eSales',
     'url'         => 'www.oxid-esales.com',
     'email'       => 'info@oxid-esales.com',
