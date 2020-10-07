ebecf8d987b31e9f18d3ed5fe30829cbc41343ca
Aneliya Lambreva <aneliya.lambreva@dnhsoft.com>
2020-10-07 13:59:44 +0300

    OXDEV-3921 OXDEV-3979 Simplify schema.sh skript

--- schema.sh
+++ schema.sh
@@ -20,6 +20,7 @@ git clone https://github.com/OXID-eSales/graphql-catalogue-module.git
 cd graphql-catalogue-module
 git checkout $(git tag | tail -1)
 composer require oxid-esales/oxideshop-db-views-generator
+composer require oxid-esales/oxideshop-composer-plugin:^v5.0.1
 composer update $DEFAULT_COMPOSER_FLAGS
 npm install -g @2fd/graphdoc
 cp $TRAVIS_BUILD_DIR/schema_database.php ./
@@ -29,9 +30,7 @@ MODULE_DIR=$(pwd);
 
 sudo sed -e 's|utf8_unicode_ci|latin1_general_ci|g; s|utf8|latin1|g' --in-place /etc/mysql/my.cnf
 sudo service mysql restart
-mkdir $MODULE_DIR/source && mkdir $MODULE_DIR/source/tmp
-mkdir $MODULE_DIR/var && mkdir $MODULE_DIR/var/configuration
-mkdir $MODULE_DIR/var/configuration/shops && touch $MODULE_DIR/var/configuration/shops/1.yaml
+
 echo -e "imports:" > $MODULE_DIR/var/configuration/configurable_services.yaml
 echo -n "    - { resource:" >> $MODULE_DIR/var/configuration/configurable_services.yaml
 echo  " SET_PATH_HERE_A }" >> $MODULE_DIR/var/configuration/configurable_services.yaml
@@ -39,14 +38,14 @@ echo -n "    - { resource:" >> $MODULE_DIR/var/configuration/configurable_servic
 echo  " SET_PATH_HERE_B }" >> $MODULE_DIR/var/configuration/configurable_services.yaml
 sed -i "s|\SET_PATH_HERE_A|\'$MODULE_DIR/services.yaml'|" $MODULE_DIR/var/configuration/configurable_services.yaml
 sed -i "s|\SET_PATH_HERE_B|\'$MODULE_DIR/vendor/oxid-esales/graphql-base/services.yaml'|" $MODULE_DIR/var/configuration/configurable_services.yaml
-cd $MODULE_DIR/vendor/oxid-esales/oxideshop-ce
-cp source/config.inc.php.dist source/config.inc.php
+
 sed -i 's|<dbHost>|localhost|; s|<dbName>|oxideshop|; s|<dbUser>|root|; s|<dbPwd>||; s|<sShopURL>|http://localhost|; s|<sShopDir>|'$MODULE_DIR'/source|; s|<sCompileDir>|'$MODULE_DIR'/source/tmp|' source/config.inc.php
 sed -i "s|\$this->edition = ''|\$this->edition = 'CE'|" source/config.inc.php
 sed -i "s|\$this->sCompileDir = ''|\$this->sCompileDir = '$MODULE_DIR/source/tmp'|" source/config.inc.php
 sed -i "s|\INSTALLATION_ROOT_PATH . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR|\'$MODULE_DIR/vendor/'|" source/bootstrap.php
-cp source/config.inc.php $MODULE_DIR/source
-cd $MODULE_DIR
+cp source/config.inc.php $MODULE_DIR/vendor/oxid-esales/oxideshop-ce/source/config.inc.php
+cp source/bootstrap.php $MODULE_DIR/vendor/oxid-esales/oxideshop-ce/source/bootstrap.php
+
 php -S localhost:8080 &
 php schema_database.php
 
