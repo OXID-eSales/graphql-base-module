2e3771c74690eca84d468a300003ededc053c774
Tomasz Kowalewski <t.kowalewski@createit.pl>
2020-11-04 11:42:57 +0100

    OXDEV-4142 Fix codestyle

--- tests/Codeception/_support/Helper/Acceptance.php
+++ tests/Codeception/_support/Helper/Acceptance.php
@@ -9,23 +9,27 @@ declare(strict_types=1);
 
 namespace OxidEsales\GraphQL\Base\Tests\Codeception\Helper;
 
+use Exception;
 use OxidEsales\Facts\Facts;
 
 class Acceptance extends \Codeception\Module
 {
     public function _beforeSuite($settings = []): void // phpcs:ignore PSR2.Methods.MethodDeclaration.Underscore
     {
-        $rootPath = (new Facts())->getShopRootPath();
+        $rootPath      = (new Facts())->getShopRootPath();
         $possiblePaths = [
             '/bin/oe-console',
             '/vendor/bin/oe-console',
         ];
+
         foreach ($possiblePaths as $path) {
             if (is_file($rootPath . $path)) {
                 exec($rootPath . $path . ' oe:module:activate oe_graphql_base');
+
                 return;
             }
         }
-        throw new \Exception('Could not find script "/bin/oe-console" to activate module');
+
+        throw new Exception('Could not find script "/bin/oe-console" to activate module');
     }
 }
