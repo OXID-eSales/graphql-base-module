c07edf770d0fed479a0e58143eed59bddd92906e
Aneliya Lambreva <aneliya.lambreva@dnhsoft.com>
2021-06-16 11:20:37 +0300

    OXDEV-4738 Get shop edition from facts

--- tests/Integration/EnterpriseTestCase.php
+++ tests/Integration/EnterpriseTestCase.php
@@ -9,11 +9,15 @@ declare(strict_types=1);
 
 namespace OxidEsales\GraphQL\Base\Tests\Integration;
 
+use OxidEsales\Facts\Facts;
+
 abstract class EnterpriseTestCase extends TokenTestCase
 {
     protected function setUp(): void
     {
-        if ($this->getConfig()->getEdition() !== 'EE') {
+        $facts = new Facts();
+
+        if ($facts->getEdition() !== 'EE') {
             $this->markTestSkipped('Skip EE related tests for CE/PE edition');
 
             return;
