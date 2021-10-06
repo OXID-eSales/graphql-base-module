72cdc0f3f94f49b4fe4be504d9756e88c43c8560
Anton Fedurtsya <anton@fedurtsya.com>
2021-10-06 17:01:20 +0300

    Simplify Token service tests

--- tests/Unit/Service/TokenTest.php
+++ tests/Unit/Service/TokenTest.php
@@ -21,110 +21,44 @@ use Symfony\Component\EventDispatcher\EventDispatcher;
 
 class TokenTest extends BaseTestCase
 {
-    /** @var LegacyService|MockObject */
-    private $legacy;
-
-    /** @var TokenService */
-    private $tokenService;
-
-    public function setUp(): void
-    {
-        $this->legacy = $this->getMockBuilder(LegacyService::class)
-            ->disableOriginalConstructor()
-            ->getMock();
-
-        $jwtConfigurationBuilder = new JwtConfigurationBuilder(
-            $this->getKeyRegistryMock(),
-            $this->legacy
-        );
-
-        $this->tokenService = new TokenService(
-            null,
-            $jwtConfigurationBuilder,
-            $this->legacy,
-            new EventDispatcher()
-        );
-    }
-
     public function testCreateTokenWithInvalidCredentials(): void
     {
-        $this->expectException(InvalidLogin::class);
-        $this->legacy
-            ->method('login')
-            ->willThrowException(new InvalidLogin('Username/password combination is invalid'));
+        $legacy = $this->createPartialMock(LegacyService::class, ['login']);
+        $legacy->method('login')->willThrowException(new InvalidLogin('Username/password combination is invalid'));
 
-        $this->tokenService->createToken('foo', 'bar');
+        $this->expectException(InvalidLogin::class);
+        $this->getTokenService($legacy)->createToken('foo', 'bar');
     }
 
     public function testCreateTokenWithValidCredentials(): void
     {
-        $userModel = $this->getUserModelStub('the_admin_oxid');
+        $legacy = $this->createPartialMock(LegacyService::class, ['login']);
+        $legacy->method('login')->willReturn($this->getUserModelStub('the_admin_oxid'));
 
-        $this->legacy
-            ->method('login')
-            ->willReturn(new User($userModel));
-        $this->legacy
-            ->method('getShopUrl')
-            ->willReturn('https://whatever.com');
-        $this->legacy
-            ->method('getShopId')
-            ->willReturn(1);
+        $token = $this->getTokenService($legacy)->createToken('admin', 'admin');
 
-        $token = $this->tokenService->createToken('admin', 'admin');
-
-        $this->assertInstanceOf(
-            Token::class,
-            $token
-        );
+        $this->assertInstanceOf(Token::class, $token);
     }
 
     public function testCreateTokenWithValidCredentialsForBlockedUser(): void
     {
-        $userModel = $this->getUserModelStub('the_admin_oxid');
+        $legacy = $this->createPartialMock(LegacyService::class, ['login', 'getUserGroupIds']);
+        $legacy->method('login')->willReturn($this->getUserModelStub('the_admin_oxid'));
+        $legacy->method('getUserGroupIds')->willReturn(['foo', 'oxidblocked', 'bar']);
 
-        $this->legacy
-            ->method('login')
-            ->willReturn(new User($userModel));
-        $this->legacy
-            ->method('getShopUrl')
-            ->willReturn('https://whatever.com');
-        $this->legacy
-            ->method('getShopId')
-            ->willReturn(1);
-        $this->legacy
-            ->method('getUserGroupIds')
-            ->willReturn(['foo', 'oxidblocked', 'bar']);
+        $token = $this->getTokenService($legacy)->createToken('admin', 'admin');
 
-        $token = $this->tokenService->createToken('admin', 'admin');
-
-        $this->assertInstanceOf(
-            Token::class,
-            $token
-        );
+        $this->assertInstanceOf(Token::class, $token);
     }
 
     public function testCreateAnonymousToken(): void
     {
-        $this->legacy->method('login')->willReturn(
-            new User($this->getUserModelStub(), true)
-        );
+        $legacy = $this->createPartialMock(LegacyService::class, ['login']);
+        $legacy->method('login')->willReturn($this->getUserModelStub());
 
-        $this->legacy
-            ->method('getShopUrl')
-            ->willReturn('https://whatever.com');
-        $this->legacy
-            ->method('getShopId')
-            ->willReturn(1);
+        $anonymousToken = $this->getTokenService($legacy)->createToken();
 
-        $anonymousToken = $this->tokenService->createToken();
-
-        $this->assertInstanceOf(
-            Token::class,
-            $anonymousToken
-        );
-
-        $this->assertEmpty(
-            $anonymousToken->claims()->get(TokenService::CLAIM_USERNAME)
-        );
+        $this->assertInstanceOf(Token::class, $anonymousToken);
+        $this->assertEmpty($anonymousToken->claims()->get(TokenService::CLAIM_USERNAME));
     }
 }
