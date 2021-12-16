11083dc1fd854b89526d328eda3cba462744585b
j.skoczek <j.skoczek@createit.pl>
2021-12-16 11:32:37 +0100

    fixes after merge

--- tests/Codeception/Acceptance/TokenCest.php
+++ tests/Codeception/Acceptance/TokenCest.php
@@ -21,6 +21,12 @@ class TokenCest
 {
     private const TEST_USER_ID = 'e7af1c3b786fd02906ccd75698f4e6b9';
 
+    private const ADMIN_LOGIN = 'admin';
+
+    private const ADMIN_PASSWORD = 'admin';
+
+    private const USER_LOGIN = 'user@oxid-esales.com';
+
     public function _before(AcceptanceTester $I): void
     {
         $this->adminDeletesAllUserTokens($I);
