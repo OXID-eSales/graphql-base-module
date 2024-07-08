5bf19a17879a844bcd5636e1e7f747ccdb1681ac
Anton Fedurtsya <anton@fedurtsya.com>
2024-07-08 10:49:09 +0300

    OXDEV-7893 Add HeaderServiceInterface

---
+++ src/Service/HeaderServiceInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+namespace OxidEsales\GraphQL\Base\Service;
+
+interface HeaderServiceInterface
+{
+    public function cleanCurrentHeaders();
+}
