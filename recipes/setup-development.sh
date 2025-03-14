1e7cd3e072b2494c0bb39e8bcab5cb47014ce740
marcelmanzel <marcel.manzel@oxid-esales.com>
2025-03-14 15:43:32 +0100

    OXDEV-8623 set specific documentation branch in recipe

--- recipes/setup-development.sh
+++ recipes/setup-development.sh
@@ -43,7 +43,7 @@ $SCRIPT_PATH/parts/shared/require.sh -n"oxid-esales/developer-tools" -v"dev-b-7.
 $SCRIPT_PATH/parts/shared/require.sh -n"oxid-esales/oxideshop-doctrine-migration-wrapper" -v"dev-b-7.3.x"
 $SCRIPT_PATH/parts/shared/require_theme_dev.sh -t"apex" -b"b-7.3.x"
 
-git clone https://github.com/OXID-eSales/oxapi-documentation source/documentation/oxapi-documentation
+git clone -b 11.0-en https://github.com/OXID-eSales/oxapi-documentation source/documentation/oxapi-documentation
 make docpath=./source/documentation/oxapi-documentation addsphinxservice
 
 make up
