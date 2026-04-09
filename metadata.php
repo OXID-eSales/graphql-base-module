56767d458709be7222d9cfbda74d1129bcc757f4
marcelmanzel <marcel.manzel@oxid-esales.com>
2026-04-09 21:31:11 +0200

    OXDEV-9909: Add a new SeoEncoderArticle model extension including factory

--- metadata.php
+++ metadata.php
@@ -10,6 +10,9 @@ declare(strict_types=1);
 /**
  * Metadata version
  */
+
+use OxidEsales\GraphQL\Base\Seo\Infrastructure\Model\SeoEncoderArticle;
+
 $sMetadataVersion = '2.0';
 
 /**
@@ -31,6 +34,7 @@ $aModule = [
     'url'         => 'www.oxid-esales.com',
     'email'       => 'info@oxid-esales.com',
     'extend'      => [
+        \OxidEsales\Eshop\Application\Model\SeoEncoderArticle::class => SeoEncoderArticle::class,
     ],
     'controllers' => [
         // Widget Controller
