309965c99affa195815193e382a7d39d00b71295
RahatHameed <therahatrajpoot@gmail.com>
2026-07-13 14:52:57 +0200

    OXDEV-10298 Add strict_types declaration to moved SEO factory interfaces

--- src/Shared/Infrastructure/Factory/Seo/SeoEncoderCategoryFactoryInterface.php
+++ src/Shared/Infrastructure/Factory/Seo/SeoEncoderCategoryFactoryInterface.php
@@ -5,6 +5,8 @@
  * See LICENSE file for license details.
  */
 
+declare(strict_types=1);
+
 namespace OxidEsales\GraphQL\Base\Shared\Infrastructure\Factory\Seo;
 
 use OxidEsales\Eshop\Application\Model\SeoEncoderCategory;
